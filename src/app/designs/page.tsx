8425bd8cbb5f390d737433130f1b1684a31a612e
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-12 19:56:04 +0530

    phase7: add saved designs page

---
+++ src/app/designs/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/AppShell";
+import { SavedDesignsClient } from "@/components/SavedDesignsClient";
+
+export default function DesignsPage() {
+  return <AppShell><div className="wrap"><SavedDesignsClient /></div></AppShell>;
+}
