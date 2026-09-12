3693eb3261facce631752fe821b5010f2d129bd2
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-12 18:21:42 +0530

    style: load Phase 4 designer styles

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,6 +1,7 @@
 import { AppShell } from "@/components/AppShell";
 import { DesignerJourney } from "@/components/DesignerJourney";
 import "./designer.css";
+import "./phase4.css";
 
 export default function Designer() {
   return (
