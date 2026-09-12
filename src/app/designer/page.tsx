8c031a05c955dc5e7077848736def993031dbaa3
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-12 18:17:38 +0530

    feat: route designer through guided journey

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,11 +1,11 @@
 import { AppShell } from "@/components/AppShell";
-import { FabricStudio } from "@/components/FabricStudio";
+import { DesignerJourney } from "@/components/DesignerJourney";
 
 export default function Designer() {
   return (
     <AppShell>
       <div className="wrap">
-        <FabricStudio />
+        <DesignerJourney />
       </div>
     </AppShell>
   );
