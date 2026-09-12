a8794eee8b8be656dcc0c6f22f5e072becc9440d
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-12 18:18:11 +0530

    style: load designer journey styles

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,5 +1,6 @@
 import { AppShell } from "@/components/AppShell";
 import { DesignerJourney } from "@/components/DesignerJourney";
+import "./designer.css";
 
 export default function Designer() {
   return (
