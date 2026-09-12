c064204aac9f31268930a9aee0b470e044368d33
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-12 19:49:38 +0530

    style: load Phase 6 visualization styles

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -2,6 +2,7 @@ import { AppShell } from "@/components/AppShell";
 import { DesignerJourney } from "@/components/DesignerJourney";
 import "./designer.css";
 import "./phase4.css";
+import "./visualization.css";
 
 export default function Designer() {
   return (
