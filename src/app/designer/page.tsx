275608bdd0b146a035513169106a8f3aeb95764d
slokpatel-crypto <slok.patel@somaiya.edu>
2026-09-14 21:00:16 +0530

    Wire designer experience enhancements

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,13 +1,14 @@
 import { AppShell } from "@/components/AppShell";
 import { DesignerJourney } from "@/components/DesignerJourney";
 import "./designer.css";
+import "./enhancements.css";
 import "./phase4.css";
 import "./visualization.css";
 
 export default function Designer() {
   return (
     <AppShell>
-      <div className="wrap">
+      <div className="wrap designerJourneyShell">
         <DesignerJourney />
       </div>
     </AppShell>
